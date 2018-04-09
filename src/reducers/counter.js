5f5397250cc5d39585bc9cc7773a3dc5aa73fac6
Rostyslav Hutii <burbigo123@gmail.com>
2018-04-09 22:51:12 +0300

    added reducers

---
+++ src/reducers/counter.js
@@ -0,0 +1,31 @@
+import {
+  INCREMENT,
+  DECREMENT,
+  INCREMENT_ASYNC,
+  CANCEL_INCREMENT_ASYNC,
+  COUNTDOWN_TERMINATED,
+} from '../actions';
+
+export const countdown = (state = 0, action) => {
+  console.log(action.type)
+  switch (action.type) {
+    case INCREMENT_ASYNC:
+      return action.payload
+    case COUNTDOWN_TERMINATED:
+    case CANCEL_INCREMENT_ASYNC:
+      return 0
+    default:
+      return state
+  }
+}
+
+export const counter = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1
+    case DECREMENT:
+      return state - 1
+    default:
+      return state
+  }
+}
