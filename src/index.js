089a5f9014ee27d64918430f3fc65c5a6823018f
Rostyslav Hutii <burbigo123@gmail.com>
2018-04-09 22:52:36 +0300

    created and initialized store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducer from './reducers';
+import rootSaga from './sagas';
+import Counter from './components/Counter';
+
+import './index.css'; 
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    reducer, 
+    applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga)
+
+render(
+    <Provider store={store}>
+        <Counter />
+    </Provider>,
+    document.getElementById('root'),
+);
